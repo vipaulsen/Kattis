7681f8c23450ebe37be3dddd723b6e92074ef270
vipaulsen <37662550+vipaulsen@users.noreply.github.com>
2021-03-10 16:14:15 +0100

    Jack-o-lantern completed

--- src/main/JackOLantern.java
+++ src/main/JackOLantern.java
@@ -1,4 +1,15 @@
 package main;
 
 public class JackOLantern {
+    public static void main(String[] args) {
+        Kattio io = new Kattio();
+        int eyeDesigns = io.getInt();
+        int noseDesigns = io.getInt();
+        int mouthDesigns = io.getInt();
+        int combinations = eyeDesigns * noseDesigns * mouthDesigns;
+
+        io.print(combinations);
+
+        io.close();
+    }
 }
