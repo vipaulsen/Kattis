0a25bbc199414a01504ca1ded2fd305bb12d9064
vipaulsen <37662550+vipaulsen@users.noreply.github.com>
2021-03-11 09:34:23 +0100

    passed, a little ugly

---
+++ src/main/BatterUp.java
@@ -0,0 +1,23 @@
+package main;
+
+public class BatterUp {
+    public static void main(String[] args) {
+        Kattio io = new Kattio();
+
+        double atBats = io.getInt();
+        double bases = 0;
+        int loopCounts = (int)atBats;
+        int add;
+        for (int i = 0; i < loopCounts; i++) {
+            add = io.getInt();
+            if(add >= 0)
+                bases+=add;
+            else
+                atBats--;
+
+        }
+
+        System.out.println(bases/atBats);
+        io.close();
+    }
+}
