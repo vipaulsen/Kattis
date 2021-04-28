5a744361c1f1df895205a6efc99aa53d2d225aff
vipaulsen <37662550+vipaulsen@users.noreply.github.com>
2021-04-28 08:56:47 +0200

    Passed, could maybe cancel some iterations when a number by itself is already larger than sumToFind

---
+++ src/main/Patuljci.java
@@ -0,0 +1,37 @@
+package main;
+
+public class Patuljci {
+    public static void main(String[] args) {
+        int[] dwarves = new int[9];
+        Kattio io = new Kattio();
+        int sum = 0;
+        for (int i = 0; i < 9; i++) {
+            int dwarfNr = io.getInt();
+            dwarves[i] = dwarfNr;
+            sum += dwarfNr;
+        }
+        int sumToFind = sum - 100;
+        int firstFalseDwarf = -1;
+        int secondFalseDwarf = -1;
+
+        for (int i = 0; i < 8; i++) {
+            for(int j = i + 1; j < 9; j++){
+                if((dwarves[i] + dwarves[j]) == sumToFind){
+                    firstFalseDwarf = dwarves[i];
+                    secondFalseDwarf = dwarves[j];
+                }
+            }
+        }
+        for (int dwarf: dwarves) {
+            if(legitDwarf(dwarf, firstFalseDwarf, secondFalseDwarf)){
+                System.out.println(dwarf);
+            }
+        }
+
+        io.close();
+    }
+
+    private static boolean legitDwarf(int dwarf, int firstFalseDwarf, int secondFalseDwarf) {
+        return !(dwarf == firstFalseDwarf || dwarf == secondFalseDwarf);
+    }
+}
