906ac46abb037eddb0b9f90cfd4e49ff44428486
vipaulsen <37662550+vipaulsen@users.noreply.github.com>
2021-04-28 09:15:25 +0200

    Added comments

--- src/main/Patuljci.java
+++ src/main/Patuljci.java
@@ -5,15 +5,19 @@ public class Patuljci {
         int[] dwarves = new int[9];
         Kattio io = new Kattio();
         int sum = 0;
+
+        // Init array
         for (int i = 0; i < 9; i++) {
             int dwarfNr = io.getInt();
             dwarves[i] = dwarfNr;
             sum += dwarfNr;
         }
+
         int sumToFind = sum - 100;
         int firstFalseDwarf = -1;
         int secondFalseDwarf = -1;
 
+        //Finding the false dwarves
         for (int i = 0; i < 8; i++) {
             for(int j = i + 1; j < 9; j++){
                 if((dwarves[i] + dwarves[j]) == sumToFind){
@@ -22,6 +26,8 @@ public class Patuljci {
                 }
             }
         }
+
+        //Printing out the legit dwarves
         for (int dwarf: dwarves) {
             if(legitDwarf(dwarf, firstFalseDwarf, secondFalseDwarf)){
                 System.out.println(dwarf);
