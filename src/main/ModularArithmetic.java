4b54b2f37cf995daae831657cd0d43fdd607dd75
vipaulsen <37662550+vipaulsen@users.noreply.github.com>
2021-05-01 14:38:35 +0200

    Sample input/output works as intended but cant get through all test cases

--- src/main/ModularArithmetic.java
+++ src/main/ModularArithmetic.java
@@ -7,12 +7,10 @@ public class ModularArithmetic {
         int operations = io.getInt();
 
         while(modulo != 0 && operations != 0){
-            //System.out.println("modulo: " + modulo + "\noperations: " + operations);
             for (int i = 0; i < operations; i++) {
                 int x = io.getInt();
                 char op = io.getWord().trim().charAt(0);
                 int y = io.getInt();
-                //System.out.println("x: " + x + "\nop: " + op + "\ny: " + y);
                 int result;
                 switch(op){
                     case '+' : result = Math.floorMod(x + y, modulo);
@@ -22,13 +20,10 @@ public class ModularArithmetic {
                     case '*' : result = Math.floorMod(x * y, modulo);
                         break;
                     case '/' :
-                        if(y==0) result = -1;
-                        else{
-                            double divRes = (x * Math.pow(y, -1));
-                            if(divRes < 1){
-                                divRes-=modulo;
-                            }
-                            result =  Math.floorMod((int)divRes, modulo);
+                        if(gcd(y, modulo) != 1){
+                            result = -1;
+                        }else{
+                            result = Math.floorMod(x*mod_inv(y, modulo), modulo);
                         }
                         break;
                     default:
@@ -39,5 +34,29 @@ public class ModularArithmetic {
             modulo = io.getInt();
             operations = io.getInt();
         }
+    }
+
+    private static int gcd(int y, int modulo) {
+        if (y == 0){
+            return modulo;
+        }
+        return gcd(modulo%y, y);
+    }
+
+    private static int mod_inv(int y, int modulo) {
+        int yy = modulo, temp, q;
+        int x0 = 0, x1 = 1;
+        if (modulo==1) return 1;
+        while (y > 1) {
+            q = y/modulo;
+            temp = modulo;
+            modulo = y % modulo;
+            y = temp;
+            temp = x0;
+            x0 =  x1 - q * x0;
+            x1 = temp;
+        }
+        if(x1 < 0) x1 += yy;
+        return x1;
     }
 }
